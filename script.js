24f89723299969585031d1995e7cb3b4a7e3d055
xxccxmb-crypto <xxccxmb@gmail.com>
2025-08-25 00:36:35 +0300

    Done

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 // Create a empty array to store the names here 
-
+let names = [];
 
 // create a function that can add names to the array and print it in the list here 
-
-
+function addName(){
+let name = document.getElementById("name").value;
+names.push(name);
+console.log(names);
+let ul = document.getElementById("namesList");
+ul.innerHTML += "<li>" + name + "</li>";
+}
 
 
 // create a function that can remove the last name from the array and print it in the list here 
-
+function removeName(){
+names.pop();
+let namesList = document.getElementById("namesList");
+namesList.removeChild(namesList.lastChild);
+console.log(names);
+}
 
 
 
