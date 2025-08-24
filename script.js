fff1f17776393926115a38275ffea62baa2e8993
alimohamedkurijan <12aligo34letsgo@gmail.com>
2025-08-24 07:25:01 +0300

    Challenge ready

---
+++ script.js
@@ -0,0 +1,15 @@
+// Create a empty array to store the names here 
+
+
+// create a function that can add names to the array and print it in the list here 
+
+
+
+
+// create a function that can remove the last name from the array and print it in the list here 
+
+
+
+
+
+
